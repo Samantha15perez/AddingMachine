249b40ffd19342f64290628a403c35012999604b
Samantha15perez <Samantha15perez@gmail.com>
2018-04-25 19:16:06 -0400

    Basic project done

---
+++ AddingMachine/AddingMachine/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace AddingMachine
+{
+    partial class AddingMachine
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxAddNumber = new System.Windows.Forms.TextBox();
+            this.labelAddedNumbers = new System.Windows.Forms.Label();
+            this.labelRunningTotal = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.buttonClear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxAddNumber
+            // 
+            this.textBoxAddNumber.Location = new System.Drawing.Point(12, 397);
+            this.textBoxAddNumber.Name = "textBoxAddNumber";
+            this.textBoxAddNumber.Size = new System.Drawing.Size(197, 20);
+            this.textBoxAddNumber.TabIndex = 0;
+            this.textBoxAddNumber.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // labelAddedNumbers
+            // 
+            this.labelAddedNumbers.BackColor = System.Drawing.Color.White;
+            this.labelAddedNumbers.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.labelAddedNumbers.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.labelAddedNumbers.Location = new System.Drawing.Point(15, 9);
+            this.labelAddedNumbers.Name = "labelAddedNumbers";
+            this.labelAddedNumbers.Size = new System.Drawing.Size(194, 335);
+            this.labelAddedNumbers.TabIndex = 2;
+            this.labelAddedNumbers.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+
+            // 
+            // labelRunningTotal
+            // 
+            this.labelRunningTotal.BackColor = System.Drawing.Color.GreenYellow;
+            this.labelRunningTotal.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.labelRunningTotal.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.labelRunningTotal.Location = new System.Drawing.Point(15, 357);
+            this.labelRunningTotal.Name = "labelRunningTotal";
+            this.labelRunningTotal.Size = new System.Drawing.Size(194, 24);
+            this.labelRunningTotal.TabIndex = 2;
+            this.labelRunningTotal.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(32, 423);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(74, 35);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Add";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.Location = new System.Drawing.Point(112, 423);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(75, 35);
+            this.buttonClear.TabIndex = 3;
+            this.buttonClear.Text = "Reset";
+            this.buttonClear.UseVisualStyleBackColor = true;
+            this.buttonClear.Click += new System.EventHandler(this.buttonClear_Click);
+            // 
+            // AddingMachine
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(223, 464);
+            this.Controls.Add(this.buttonClear);
+            this.Controls.Add(this.labelRunningTotal);
+            this.Controls.Add(this.labelAddedNumbers);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBoxAddNumber);
+            this.Name = "AddingMachine";
+            this.Text = "AddingMachine";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxAddNumber;
+        private System.Windows.Forms.Label labelAddedNumbers;
+        private System.Windows.Forms.Label labelRunningTotal;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button buttonClear;
+    }
+}
+
---
+++ AddingMachine/AddingMachine/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AddingMachine
+{
+    public partial class AddingMachine : Form
+    {
+        //creates the array
+        
+        double[] AddingMachineArray = new double[10];
+
+        public AddingMachine()
+        {
+            InitializeComponent();
+        }
+
+
+        
+        int i = 0;
+        //this variable handles the placement of elements within the array
+        string PreviousNumbers = "";
+        //setting the previous numbers string as a variable helps things along smoothly when you reset the array
+        double total = 0;
+        //setting the running total as a variable helps things along smoothly when you reset the array
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            textBoxAddNumber.Focus();
+            //sets the focus immediately to the text box when the form loads
+            try
+            {            
+                if (Double.Parse(textBoxAddNumber.Text) > 0)
+                {                
+                    AddingMachineArray[i] = Double.Parse(textBoxAddNumber.Text);
+                    //sets the value of the first element in the array to whatever is in the input box
+                    total = total + AddingMachineArray[i];
+                    //puts the sum of the array into a variable
+                    PreviousNumbers = PreviousNumbers + AddingMachineArray[i].ToString() + ", " + System.Environment.NewLine;
+                    //adds the current number to the list of previous numbers                    
+                    labelAddedNumbers.Text = PreviousNumbers;
+                    //prints the list
+                    labelRunningTotal.Text = total.ToString();    
+                    //prints the total to the textbox
+                    textBoxAddNumber.Text = "";
+                    //blanks out the text box once you hit the button
+                    i++;
+                    //shifts the position of the array foreward by one. 
+                }
+                else
+                {
+                    //error handling if the number is not positive
+                    MessageBox.Show("Please only use positive numbers!");
+                    textBoxAddNumber.Text = "";
+                }
+
+                if (i > (AddingMachineArray.Length - 1))
+                {
+                    //when you reach the end of the array, it resets it and starts from the beginning 
+                    //all while mantaining your total
+                    total = AddingMachineArray.Sum();
+                    Array.Clear(AddingMachineArray, 0, AddingMachineArray.Length);
+                    i = 0;
+
+                }
+            }
+            catch (Exception)
+            {
+                //error handling for everything else
+                MessageBox.Show("Numbers only, please.");
+                textBoxAddNumber.Text = "";
+            }
+        }
+        private void buttonClear_Click(object sender, EventArgs e)
+        {
+            //this resets everything
+            total = 0;
+            i = 0;
+            PreviousNumbers = "";
+            labelAddedNumbers.Text = "";
+            labelRunningTotal.Text = "";
+            Array.Clear(AddingMachineArray, 0, AddingMachineArray.Length);
+            textBoxAddNumber.Text = "";
+        }
+    }
+}
